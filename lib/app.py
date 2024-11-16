e166bd8b83906887429cfd01f76badcee3344f94
mikewen8 <mikecwen@gmail.com>
2024-11-16 13:49:53 -0800

    change

--- lib/app.py
+++ lib/app.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 CORS(app)  # Allow all origins
 
 
+logged_user=0
+def get_userID():
+    return 2
+
 def user_events():
+    #if(logged_user==user_events)
     #search aws db through python to get the id list
     return " "   
 
 
 def search_events():
     # use this to find the event ids 
+
     return " "
 
 # need to have a script to upload the fetch data over time
@@ -23,6 +29,7 @@ def get_events():
     # Example: Fetch event data
     # need to call the fetch from database Hardik do this! and have this search wheather it 
     # to do this we will search the db for the user
+
     # take the list of event ids and then search the events 
     return {"events": [{"id": 1, "name": "Concert", "description":"Loud and proud Tyler sucks at bedwars"}, {"id": 2, "name": "Art Exhibition", "description":"this is the event description"}]}
 
