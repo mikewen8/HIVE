da9a086abcb61412c1ee5a193302eeab4c93a4fe
mikewen8 <mikecwen@gmail.com>
2024-11-16 11:19:25 -0800

    changes have been made

---
+++ lib/pages/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
+          },
+          child: const Text('Go to In Progress Screen'),
+        ),
+      ),
+    );
+  }
+}
